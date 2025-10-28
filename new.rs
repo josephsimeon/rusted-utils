eea6f5bc39b5d299172dff5a1a6e613a7f30e0b8
Joseph Simeon <josephdmsimeon@icloud.com>
2025-10-29 05:05:44 +1000

    Added in a check for FileStream before proceeding to wc processing in new.rs (rusted-wc)

--- new.rs
+++ new.rs
@@ -1,6 +1,7 @@
 // @file    rusted-wc.rs
 // @author  Joseph Simeon
 // @brief   `wc` command-line rewritten with Rust
+use std::process;
 
 #[derive(Debug)] // TODO delete when finsihed
 struct FileStream {
@@ -16,14 +17,14 @@ impl FileStream {
         }
     }
     
-    fn filestream_error(errs: &str) {
+    fn filestream_error(errs: String) {
         eprintln!("{errs}");
         process::exit(1);
     }
 
     fn update(&mut self, args: Vec<String>) -> &Self {
         if args.is_empty() {
-            FileStream::filestream_error("Error in FileStream, no arguments given");
+            FileStream::filestream_error(format!("Error in FileStream, no arguments given"));
         }
     
         let mut arg_iter = args.iter();
@@ -46,6 +47,23 @@ impl FileStream {
         self.names.push(String::from(new));
         self
     }
+    
+    fn check(&self) {
+        for f in self.flags.chars() {
+            match f {
+                '-' | 'l' | 'w' | 'c' | 'm' | 'L' => {},
+                _ => {
+                    FileStream::filestream_error(format!("Error in FileStream illegal flag for rusted-wc: {}", f));
+                    process::exit(1);
+                },
+            }
+        }
+
+        if self.names.is_empty() {
+            FileStream::filestream_error(format!("Error in FileStream no file(s) to target with rusted-wc"));
+            process::exit(1);
+        }
+    }
 }
 
 fn main() {
@@ -54,5 +72,6 @@ fn main() {
 
     let mut fs = FileStream::new();
     fs.update(args);
+    fs.check();
     println!("fs = {fs:?}"); // TODO delete when finished
 }
