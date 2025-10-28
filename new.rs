c129f660c7ab4bdce56b11b0ad22c7aa5b5385ca
Joseph Simeon <josephdmsimeon@icloud.com>
2025-10-29 04:06:28 +1000

    Added some simple content to the new functions to test that they work in new.rs (rusted-wc)

--- new.rs
+++ new.rs
@@ -2,6 +2,7 @@
 // @author  Joseph Simeon
 // @brief   `wc` command-line rewritten with Rust
 
+#[derive(Debug)] // TODO delete when finsihed
 struct FileStream {
     flags: String,
     names: Vec<String>,
@@ -16,15 +17,26 @@ impl FileStream {
     }
 
     fn update(&mut self, args: Vec<String>) -> &Self {
+        self.update_flags(args[0].clone());
+        self.update_filenames(args[1].clone());
+        self
     }
 
     fn update_flags(&mut self, new: String) -> &Self {
+        self.flags = new;
+        self
     }
 
     fn update_filenames(&mut self, new: String) -> &Self {
+        self.files.push(new);
+        self
     }
 }
 
 fn main() {
+    // TODO let args: Vec<String> = env::args().skip(1).collect();
+
     let mut fs = FileStream::new();
+    fs.update(vec!["-w", "README.md"]);
+    println!("fs = {fs:?}"); // TODO delete when finished
 }
