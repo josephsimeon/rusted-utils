58a2b35b6ebcf7e8477a6f2d8203d595381ff3a5
Joseph Simeon <josephdmsimeon@icloud.com>
2025-10-29 03:51:14 +1000

    Added the starting struct FileStream and empty main in new.rs (rusted-wc)

--- new.rs
+++ new.rs
@@ -1,3 +1,11 @@
 // @file    rusted-wc.rs
 // @author  Joseph Simeon
 // @brief   `wc` command-line rewritten with Rust
+
+struct FileStream {
+    flags: String,
+    names: Vec<String>,
+}
+
+fn main() {
+}
