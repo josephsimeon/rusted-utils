5002d6288eeebd09d19f4f2f4f013c72dd7d0a6f
Joseph Simeon <josephdmsimeon@icloud.com>
2025-11-01 05:47:30 +1000

    Added in the building and returned the 'struct' based on the 'args' given, added TODOs for errors that may occur

--- rusted-wc/src/lib.rs
+++ rusted-wc/src/lib.rs
@@ -2,8 +2,31 @@ struct FileStream {
     flags: String,
     filenames: Vec<String>,
 }
+
 impl FileStream {
     fn build(args: Vec<String>) -> FileStream {
+        // check for empty args
+        if args.is_empty() {
+            // TODO add in a process::exit routine and error print for empty argument
+        }
+
+        let mut arg_iter = args.iter();
+    
+        // process flags section of the args
+        let flags: String;
+        match args[0].starts_with("-") {
+            true => flags = arg_iter.next().unwrap().to_string(),
+            false => flags = "".to_string(),
+        }
+
+        // check for empty args before processing filenames
+        if arg_iter.is_empty() {
+            // TODO add in a process::exit routine and error print for no file argument
+        }
+
+        let filenames: Vec<String> = arg_iter.map(|s| s.to_string()).collect();
+
+        FileStream { flags, filenames}
     }
 }
 
