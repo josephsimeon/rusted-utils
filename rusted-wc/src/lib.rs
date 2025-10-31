1f87e4d5d2031c1bf9f77314959add5d3dbc6b91
Joseph Simeon <josephdmsimeon@icloud.com>
2025-11-01 05:46:35 +1000

    Added in a test for the 'build' constructor for 'struct FileStream'

--- rusted-wc/src/lib.rs
+++ rusted-wc/src/lib.rs
@@ -6,3 +6,19 @@ impl FileStream {
     fn build(args: Vec<String>) -> FileStream {
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_build() {
+        let test = FileStream::build(vec![
+            "-w".to_string(),
+            "README.md".to_string(),
+        ]);
+
+        assert_eq!(test.flags, "-w".to_string());
+        assert_eq!(test.filenames, vec!["README.md".to_string()]);
+    }
+}
