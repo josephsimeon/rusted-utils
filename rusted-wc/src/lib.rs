d58904d8c242cdd34eca738eaa6d683165bac1ab
Joseph Simeon <josephdmsimeon@icloud.com>
2025-11-02 03:04:21 +1000

    Changed flags to be a vector in case of multiple seperated flags

--- rusted-wc/src/lib.rs
+++ rusted-wc/src/lib.rs
@@ -1,6 +1,6 @@
 #[derive(Debug)]
 struct FileStream {
-    flags: String,
+    flags: Vec<String>,
     filenames: Vec<String>,
 }
 
@@ -11,34 +11,40 @@ impl FileStream {
             return Err(format!("rusted-wc: error: no arguments given"));
         }
 
-        let mut arg_iter = args.iter();
-    
-        // process flags section of the args
-        let flags: String;
-        match args[0].starts_with('-') {
-            true => flags = arg_iter.next().unwrap().to_string(),
-            false => flags = "".to_string(),
-        }
+        // shadow args with a mut reference
+        let mut args: Vec<String> = args.into_iter().collect();
 
-        if !flags.is_empty() {
-            // check that flags contain legal options for rusted-wc
-            if flags == "-".to_string() {
-                return Err(format!("rusted-wc: error: no flag information '-'"));
-            }
+        // create a vector that holds all flags
+        let flags: Vec<String> = args
+            .iter()                                     // iterate through
+            .filter(|&word| word.starts_with('-'))      // filter viable flags
+            .map(|word| word.to_string())               // translate from &String to String
+            .collect();                                 // create the new collection of String
 
-            for f in flags.chars() {
-                if !flags_ok.contains(f) {
-                    return Err(format!("rusted-wc: error: illegal option '{}'", f));
+        // retain non viable flags
+        args.retain(|word| !word.starts_with('-'));
+
+        if flags.len() > 0 {
+            // check that flags contain at least an option for rusted-wc
+            for flag in &flags {
+                if *flag == "-".to_string() {
+                    return Err(format!("rusted-wc: error: no flag information '-'"));
+                }
+
+                for f in flag.chars() {
+                    if !flags_ok.contains(f) {
+                        return Err(format!("rusted-wc: error: illegal option '{}', usage [-Lclmw]", f));
+                    }
                 }
             }
         }
 
         // check for empty args before processing filenames
-        if arg_iter.len() == 0 {
+        if args.len() == 0 {
             return Err(format!("rusted-wc: error: no filename arguments given"));
         }
 
-        let filenames: Vec<String> = arg_iter.map(|s| s.to_string()).collect();
+        let filenames: Vec<String> = args.into_iter().collect();
 
         Ok(FileStream { flags, filenames })
     }
@@ -57,7 +63,7 @@ mod test {
         let flags: String = "-lwcmL".to_string();
         let test = FileStream::build(flags, vec).unwrap();
 
-        assert_eq!(test.flags, "-w".to_string());
+        assert_eq!(test.flags, vec!["-w".to_string()]);
         assert_eq!(test.filenames, vec!["README.md".to_string()]);
     }
 
@@ -69,7 +75,7 @@ mod test {
         let flags: String = "-lwcmL".to_string();
         let test = FileStream::build(flags, vec).unwrap();
 
-        assert_eq!(test.flags, "".to_string());
+        assert_eq!(test.flags, Vec::<String>::new());
         assert_eq!(test.filenames, vec!["README.md".to_string()]);
     }
 
@@ -83,7 +89,7 @@ mod test {
         let flags: String = "-lwcmL".to_string();
         let test = FileStream::build(flags, vec).unwrap();
 
-        assert_eq!(test.flags, "-w".to_string());
+        assert_eq!(test.flags, vec!["-w".to_string()]);
         assert_eq!(test.filenames, vec!["README.md".to_string(), "README.md".to_string()]);
     }
 
