2342abbcc7de0a9200d9fd8fb1ad68b8980960f1
Joseph Simeon <josephdmsimeon@icloud.com>
2025-11-17 21:35:44 +1000

    Added new function in `tests/cli.rs` called `runs` which just checks the `stdout` of the binary which is still the usual `cargo new` boilerplate

---
+++ r-wc/tests/cli.rs
@@ -0,0 +1,13 @@
+use assert_cmd::cargo;
+
+#[test(cfg)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn runs() {
+        let cmd = cargo::cargo_bin_cmd!("r-wc").unwrap();
+        let stdout = String::from_utf8(cmd.stdout).expect("invalid UTF-8");
+        assert_eq!(stdout, "Hello, world!\n");
+    }
+}
