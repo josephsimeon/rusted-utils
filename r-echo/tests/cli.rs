213685e5fd892b85e061e330f85cb812c1f29153
Joseph Simeon <josephdmsimeon@icloud.com>
2025-11-18 13:53:02 +1000

    Added `tests/` directory with integration test called `cli.rs`

---
+++ r-echo/tests/cli.rs
@@ -0,0 +1,14 @@
+use assert_cmd::cargo;
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn runs() {
+        let cmd = cargo::cargo_bin_cmd!("echor").unwrap();
+        let stdout = String::from_utf8(cmd.stdout).expect("invalid UTF-8");
+        assert_eq!(stdout, "Hello, world!\n");
+    }
+}
+
