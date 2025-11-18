2804fa6de8baefd9376d3f95eadc7c21b4534de9
Joseph Simeon <josephdmsimeon@icloud.com>
2025-11-18 16:46:10 +1000

    Added `WordCount` `struct` in `src/main.rs` to hold the values of the processed count, as well as added a `new` constructor in an `impl` block for `WordCount` as well as a unit test for the `new` constructor

--- r-wc/src/main.rs
+++ r-wc/src/main.rs
@@ -31,8 +31,44 @@ struct Cli {
     words: bool,
 }
 
+#[derive(Debug, PartialEq, Clone)]
+pub struct WordCount {
+    lines: usize,
+    words: usize,
+    letters: (usize, usize),
+    longest: (usize, usize),
+}
+
+impl WordCount {
+    fn new() -> WordCount {
+        WordCount {
+            lines: 0,
+            words: 0,
+            letters: (0, 0),
+            longest: (0, 0),
+        }
+    }
+}
+
 fn main() {
     let cli = Cli::parse();
 
     println!("{cli:?}");
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_wc_new() {
+        let test = WordCount {
+            lines: 0,
+            words: 0,
+            letters: (0, 0),
+            longest: (0, 0),
+        };
+
+        assert_eq!(WordCount::new(), test);
+   }
 }
