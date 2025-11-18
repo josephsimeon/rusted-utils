40c9c808148bf5cf420d2648aeab03a1770963db
Joseph Simeon <josephdmsimeon@icloud.com>
2025-11-18 15:29:19 +1000

    Added in the `Cli` struct in `src/main.rs` that records the expected args that will be processed by `r-wc` as well as documentation about the command-line app that is shown in `-h` or `--help` flagalong with printing out the `Cli` struct for debugging purposes

--- r-wc/src/main.rs
+++ r-wc/src/main.rs
@@ -1,3 +1,38 @@
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[command(author, version, about)]
+/// Rust version of `wc`
+///
+/// Utility to display the number of lines, words, and bytes contained in each input file by
+/// default.
+struct Cli {
+    /// File input to be processed by `r-wc`
+    file: Vec<String>,
+
+    /// Print longest length in the input file in characters or bytes
+    #[arg(short('L'))]
+    length: bool,
+
+    /// Print number of characters in the input file
+    #[arg(short('c'))]
+    bytes: bool,
+
+    /// Print number of lines in the input file
+    #[arg(short('l'))]
+    lines: bool,
+
+    /// Print number of bytes in the input file
+    #[arg(short('m'))]
+    chars: bool,
+
+    /// Print number of words in the input file
+    #[arg(short('w'))]
+    words: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    println!("{cli:?}");
 }
