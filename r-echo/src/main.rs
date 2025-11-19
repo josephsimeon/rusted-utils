102d64c41f0b28b03248a21228d78c23b8397aa0
Joseph Simeon <josephdmsimeon@icloud.com>
2025-11-20 02:26:38 +1000

    Added content for the `clap` to parse arguments into `struct Cli`

--- r-echo/src/main.rs
+++ r-echo/src/main.rs
@@ -1,3 +1,23 @@
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[command(author, version, about)]
+/// Rust version of `echo`
+/// 
+/// Utility wriates any specified operands seperated by a single blank (' ') character and followed
+/// by a newline ('\n') character to the standard output.
+struct Cli {
+    #[arg(required(true))]
+    /// Input operand processed by `r-echo`.
+    string: Vec<String>,
+
+    #[arg(short('n'))]
+    /// Do not print the trailing newline character.
+    no_newline: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    println!("{cli:?}"); // TODO debug purposes
 }
